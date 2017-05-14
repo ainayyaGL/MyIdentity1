b585690758067995a6da48e8852b184baa8fb106
ainayyaGL <16523131@students.uii.ac.id>
2017-05-14 13:31:33 +0700

    tak tambahin yg dosen

--- MyIdentity1/src/myidentity1/FXMLDocumentController.java
+++ MyIdentity1/src/myidentity1/FXMLDocumentController.java
@@ -15,6 +15,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.Button;
 import javafx.scene.Group;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.ScrollPane;
 
 
 /**
@@ -33,11 +35,16 @@ public class FXMLDocumentController implements Initializable {
     private PasswordField tfPassAd, tfPassCo;
     
     @FXML
-    private Button btAdmin, btCoAdmin, btSignInAd, btSignInCo;
+    private Button btAdmin, btCoAdmin, btSignInAd, btSignInCo, go, btDosen, btKaryawan, keluar, btKembaliJ, btKembaliMenuT, btTambah, btLihat, btKembaliDataDL, btKembaliDDF;
     
     @FXML
     private Group g1, g2;
     
+    @FXML
+    private ScrollPane DataDiriF;
+    
+    @FXML
+    private AnchorPane loginForm , menuUtama, JurusanList, MenuTampil, DataList, DataDiriForm;
     
     @FXML
     public void AdminOnClick(ActionEvent event){
@@ -55,10 +62,116 @@ public class FXMLDocumentController implements Initializable {
     public void SignInAdminOnClick(ActionEvent event){
         if(tfUserAd.getText().equalsIgnoreCase("erna") && tfPassAd.getText().equalsIgnoreCase("12345")){
             label.setText("Silahkan masuk");
+            go.setVisible(true);
         } else {
             label.setText("username/password anda salah");
         }
     }
+    @FXML
+    public void goOnClick(ActionEvent event){
+        loginForm.setVisible(false);
+        menuUtama.setVisible(true);
+        JurusanList.setVisible(false);
+        MenuTampil.setVisible(false);
+        DataList.setVisible(false);
+        DataDiriForm.setVisible(false);
+        
+        
+    }
+    
+    @FXML
+    public void DosenOnClick(ActionEvent event){
+        JurusanList.setVisible(true);
+        loginForm.setVisible(false);
+        menuUtama.setVisible(false);
+        MenuTampil.setVisible(false);
+        DataList.setVisible(false);
+        DataDiriForm.setVisible(false);
+        
+    }
+    
+    @FXML
+    public void backJurusan(ActionEvent event){
+        menuUtama.setVisible(true);
+        JurusanList.setVisible(false);
+        loginForm.setVisible(false);
+        MenuTampil.setVisible(false);
+        DataList.setVisible(false);
+        DataDiriForm.setVisible(false);
+        
+    }
+    
+    @FXML
+    public void backMenuTampil(ActionEvent event){
+        MenuTampil.setVisible(false);
+        loginForm.setVisible(false);
+        menuUtama.setVisible(false);
+        JurusanList.setVisible(true);
+        DataList.setVisible(false);
+        DataDiriForm.setVisible(false);
+    }
+    
+    @FXML
+    public void keluar(ActionEvent event){
+        loginForm.setVisible(true);
+        menuUtama.setVisible(false);
+        JurusanList.setVisible(false);
+        MenuTampil.setVisible(false);
+        DataList.setVisible(false);
+        DataDiriForm.setVisible(false);
+    }
+    
+    @FXML
+    public void TIFonCLick(ActionEvent event){
+        MenuTampil.setVisible(true);
+        loginForm.setVisible(false);
+        menuUtama.setVisible(false);
+        JurusanList.setVisible(false);
+        DataList.setVisible(false);
+        DataDiriForm.setVisible(false);
+    }
+    
+    @FXML
+    public void tampilkanDataOnClick(ActionEvent event){
+        DataList.setVisible(true);
+        MenuTampil.setVisible(false);
+        loginForm.setVisible(false);
+        menuUtama.setVisible(false);
+        JurusanList.setVisible(false);
+        DataDiriForm.setVisible(false);
+    }
+    
+    @FXML
+    public void backDataD(ActionEvent event){
+        DataList.setVisible(false);
+        MenuTampil.setVisible(true);
+        loginForm.setVisible(false);
+        menuUtama.setVisible(false);
+        JurusanList.setVisible(false);
+        DataDiriForm.setVisible(false);
+    }
+    
+    @FXML
+    public void tambahOnClick(ActionEvent event){
+        DataDiriForm.setVisible(true);
+        DataList.setVisible(false);
+        MenuTampil.setVisible(false);
+        loginForm.setVisible(false);
+        menuUtama.setVisible(false);
+        JurusanList.setVisible(false);
+    }
+    
+    @FXML
+    public void backDDF(ActionEvent event){
+        DataDiriForm.setVisible(false);
+        DataList.setVisible(true);
+        MenuTampil.setVisible(false);
+        loginForm.setVisible(false);
+        menuUtama.setVisible(false);
+        JurusanList.setVisible(false);
+    }
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
